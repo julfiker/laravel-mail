e91551c8304060aad18676fe240511f4deaa17bb
Julfiker <infro.jewel@gmail.com>
2020-12-21 23:32:38 +0600

    Date Format

--- app/Models/EmailLog.php
+++ app/Models/EmailLog.php
@@ -10,6 +10,11 @@ class EmailLog extends Model
     use HasFactory;
     protected $fillable = ['mail_to', 'mail_from', 'mail_subject', 'content_html', 'content_plain'];
 
+    protected $casts = [
+        'created_at'  => 'date:Y-m-d H:i:s',
+        'updated_at' => 'datetime:Y-m-d H:i:s',
+    ];
+
     public function attachments() {
         return $this->hasMany(EmailAttachment::class, 'email_log_id');
     }
